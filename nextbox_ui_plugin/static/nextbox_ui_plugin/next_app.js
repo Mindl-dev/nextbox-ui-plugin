aaaba81e41f863ab13308f1a9dd40e114735dad0
Mindl-dev <jon@tetterton.cc>
2023-06-30 16:30:10 -0400

    Update next_app.js

--- nextbox_ui_plugin/static/nextbox_ui_plugin/next_app.js
+++ nextbox_ui_plugin/static/nextbox_ui_plugin/next_app.js
@@ -58,22 +58,12 @@
             linkType: 'curve',
             sourcelabel: 'model.srcIfName',
             targetlabel: 'model.tgtIfName',
+            color: 'model.color',
             style: function(model) {
                 if (model._data.is_dead === 'yes') {
                     return { 'stroke-dasharray': '5' }
                 }
             },
-            color: function(model) {
-                if (model._data.is_dead === 'yes') {
-                    return '#E40039'
-                }
-                if (model._data.is_new === 'yes') {
-                    return '#148D09'
-                }
-                if (model.color !== '#') {
-                    console.log(model.color);
-                    return 'model.color'
-                }
             },
         },
         // Display Node icon. Displays a dot if set to 'false'.
